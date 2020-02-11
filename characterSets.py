ec3ba001a90ac68c5b26431a5999d0d01d4722a3
Clif Wolfe <clifwolfe1@gmail.com>
2020-02-11 11:23:41 -0500

    Added emojis

--- characterSets.py
+++ characterSets.py
@@ -1,5 +1,5 @@
 
-print("Character Sets\n")
+
 
 def batch_gen(it, b):
     l = len(it)
@@ -7,8 +7,16 @@ def batch_gen(it, b):
         yield it[i:min(i + b, l)]
 
 
-for batch in batch_gen(range(0x3040, 0x30ff), 16):
-    print(' '.join([chr(i) if i not in {0x3040, 0x3097, 0x3098} else
-                    chr(0x25a1) for i in batch]))
+def printchars(start, stop, exclude=None, alt_exclude=0x25a1):
+    exclude_list = {}
+    if exclude is not None:
+        exclude_list = exclude
+    for batch in batch_gen(range(start, stop), 16):
+        print(' '.join([chr(i) if i not in exclude_list else
+                        chr(alt_exclude) for i in batch]))
 
 
+if __name__ == "__main__":
+    print("Character Sets\n")
+    printchars(0x3040, 0x30ff, {0x3040, 0x3097, 0x3098}, 0x25a1)
+    printchars(0x1f000, 0x1f8ff)
